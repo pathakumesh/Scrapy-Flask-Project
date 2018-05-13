48e584b667e81c1446e2b3f2f57dac1271d9c3ff
Umesh Pathak <umeshpd.pathak@gmail.com>
2018-05-13 13:31:20 +0545

    *.pyc removed and Home Page description updated

--- run_server.py
+++ run_server.py
@@ -17,7 +17,7 @@ app = create_app('etc.settings')
 logging.root.setLevel(logging.DEBUG)
 logging.disable(0)
 
-# app = DispatcherMiddleware(app, {'/main': app})
+app = DispatcherMiddleware(app, {'/main': app})
 
-# run_simple('0.0.0.0', 5000, app, True, True)
+run_simple('0.0.0.0', 5500, app, True, True)
 
